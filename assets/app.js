7f5e58048f869593506655d521375da3d48ecaba
Luan Santos <luansantos@crossknowledge.com>
2017-01-27 11:29:45 -0200

    first commit

---
+++ assets/app.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+  var linksCarousel = $('.owl-carousel.links')
+  var contentCarousel = $('.owl-carousel.content');
+
+  linksCarousel.owlCarousel({
+  	items:4,
+  	margin: 10,
+  	autoplay: false,
+  	nav:false,
+  	dots:false
+  });
+
+  $(".link-carousel-next").click(function () {
+  	linksCarousel.trigger('next.owl.carousel')
+  });
+  $(".link-carousel-prev").click(function () {
+  	linksCarousel.trigger('prev.owl.carousel')
+  })
+
+
+
+
+
+  contentCarousel.owlCarousel({
+  	margin: 10,
+  	items:1,
+  	URLhashListener:true,
+    autoplayHoverPause:true,
+    startPosition: 'URLHash',
+    dots:false
+  });
+});
